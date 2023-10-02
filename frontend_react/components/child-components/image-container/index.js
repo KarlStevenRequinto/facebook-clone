35ced467d552a640ec7e627d4bf13eac1966a83f
KarlStevenRequinto <lerdrobott@gmail.com>
2023-10-02 22:27:58 +0800

    modifying image-container component for icon conditions

--- frontend_react/components/child-components/image-container/index.js
+++ frontend_react/components/child-components/image-container/index.js
@@ -1,14 +1,27 @@
+import MessengerIcon from "../../../ui/svg/messenger-icon";
 import styles from "./styles.module.css";
-const ImageContainer = ({ isOnline = true }) => {
+const ImageContainer = ({ isOnline = true, isForIcon = false }) => {
   const imagePath = "/static/images/profile-picture.jpg"; //TO-DO make image a props
+  const Icon = ({ width, height }) => {
+    return (
+      <div className={styles.iconContainer}>
+        <MessengerIcon width={width} height={height} />
+      </div>
+    );
+  };
   return (
+    // TO-DO: make background color dynamic
     <div className={styles.container}>
       <div className={styles.profileImageWrapper}>
-        <img
-          src={imagePath}
-          alt="Profile Picture"
-          className={styles.profileImage}
-        />
+        {isForIcon ? (
+          <Icon width={20} height={20} />
+        ) : (
+          <img
+            src={imagePath}
+            alt="Profile Picture"
+            className={styles.profileImage}
+          />
+        )}
       </div>
       {isOnline ? (
         <div className={styles.onlineBadge}>
--- frontend_react/pages/index.js
+++ frontend_react/pages/index.js
@@ -1,7 +1,8 @@
+
 export default function Home() {
   return (
-    <body style={{ margin: 0, overflow: "hidden" }}>
-      <main
+    <div style={{ margin: 0, overflow: "hidden" }}>
+      <div
         style={{
           display: "flex",
           flexDirection: "column",
@@ -11,8 +12,9 @@ export default function Home() {
         <h3 style={{ marginTop: 0 }}>Facebook Clone Project</h3>
         <div style={{ flex: 1, display: "flex", alignItems: "center" }}>
           {/* call your component here */}
+
         </div>
-      </main>
-    </body>
+      </div>
+    </div>
   );
 }
