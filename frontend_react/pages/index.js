4dcac38880fcb8b01e494d0f60aa7fe895899b5f
KarlStevenRequinto <lerdrobott@gmail.com>
2023-10-03 21:55:27 +0800

    adding navbar button component utilizing icons

--- frontend_react/pages/index.js
+++ frontend_react/pages/index.js
@@ -1,3 +1,5 @@
+import NavButtons from "../components/child-components/buttons/nav-buttons";
+import HomeIcon from "../ui/svg/home-icon";
 
 export default function Home() {
   return (
@@ -10,9 +12,19 @@ export default function Home() {
         }}
       >
         <h3 style={{ marginTop: 0 }}>Facebook Clone Project</h3>
-        <div style={{ flex: 1, display: "flex", alignItems: "center" }}>
+        <div style={{ flex: 1, display: "flex", alignItems: "center",justifyContent:"center",backgroundColor:"blue" }}>
           {/* call your component here */}
-
+          <NavButtons
+            icon={
+              <HomeIcon
+                width={22}
+                height={22}
+                stroke={"#828282"}
+                strokeWidth={1}
+                fill={"none"}
+              />
+            }
+          />
         </div>
       </div>
     </div>
