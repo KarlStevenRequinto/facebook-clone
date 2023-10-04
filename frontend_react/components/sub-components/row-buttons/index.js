63ace6d4ad4eacae09f08284c37720d502fc8519
KarlStevenRequinto <lerdrobott@gmail.com>
2023-10-04 20:39:13 +0800

    component structure

---
+++ frontend_react/components/sub-components/row-buttons/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RowButton = ({icon,text}) => {
+  return (
+    <div>
+      <div>{icon}</div>
+      <div>{text}</div>
+    </div>
+  );
+};
+
+export default RowButton;
