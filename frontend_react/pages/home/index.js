5d10ddd21235911bed3ef77c0e17a052ad521479
KarlStevenRequinto <lerdrobott@gmail.com>
2023-11-03 05:25:37 +0800

    removed previous navigation, added Layout, used Link

--- frontend_react/pages/home/index.js
+++ frontend_react/pages/home/index.js
@@ -1,13 +1,9 @@
-import NavBar from "../../components/main-component/navbar";
-import usePageNavigation from "../../hooks/page-navigation";
-import styles from "./styles.module.css";
-
 const HomePage = () => {
-  const { navigateToPage } = usePageNavigation();
   return (
     <div>
-      <NavBar onNavBtnClick={navigateToPage} />
-      <main className="contentContainer">HomePage Content</main>
+      <main className="contentContainer">
+        <h1>HomePage Content</h1>
+      </main>
     </div>
   );
 };
--- frontend_react/pages/watch/index.js
+++ frontend_react/pages/watch/index.js
@@ -1,11 +1,6 @@
-import NavBar from "../../components/main-component/navbar";
-import usePageNavigation from "../../hooks/page-navigation";
-
 const WatchPage = () => {
-  const { navigateToPage } = usePageNavigation();
   return (
     <div>
-      <NavBar onNavBtnClick={navigateToPage}/>
       <main className="contentContainer">WatchPage Content</main>
     </div>
   );
