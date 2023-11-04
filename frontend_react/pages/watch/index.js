e12e8b01bc47d0294c59b094a44677b994e274eb
KarlStevenRequinto <lerdrobott@gmail.com>
2023-11-05 00:39:17 +0800

    added home page structure, installed bootstrap, added layout component

--- frontend_react/pages/watch/index.js
+++ frontend_react/pages/watch/index.js
@@ -1,7 +1,14 @@
+import styles from "./styles.module.css";
 const WatchPage = () => {
   return (
     <div>
-      <main className="contentContainer">WatchPage Content</main>
+      <main className="contentContainer">
+        <div className="row align-items-start">
+          <div className={`col ${styles.sideBar}`}>asdasd</div>
+          <div className={`col ${styles.centerCol}`}>Column</div>
+          <div className={`col ${styles.sideBar}`}>Column</div>
+        </div>
+      </main>
     </div>
   );
 };
