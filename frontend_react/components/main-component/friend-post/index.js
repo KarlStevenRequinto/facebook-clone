8e3fd761bd456a236c4265b7ae8e4fc61d1b5d54
KarlStevenRequinto <lerdrobott@gmail.com>
2023-12-17 04:06:07 +0800

    added header, initial progress

--- frontend_react/components/main-component/friend-post/index.js
+++ frontend_react/components/main-component/friend-post/index.js
@@ -1,14 +1,34 @@
 import React from "react";
 import styles from "./styles.module.css";
 import CardContainer from "../card-container";
+import ImageContainer from "../../child-components/image-container";
 
 const FriendPost = () => {
   return (
     <CardContainer className={styles.container}>
-      <div>Header</div>
-      <div>Body</div>
-      <div>React Box</div>
-      <div>Comment Box</div>
+      <div className={styles.postContainer}>
+        <div className={styles.headerContainer}>
+          <div className={styles.innerHeaderContainer}>
+            <div className={styles.imageContainer}>
+              {/* <img
+                src="/static/images/profile-picture.jpg"
+                width={40}
+                height={40}
+              ></img> */}
+              <ImageContainer isOnline={false} width={40} height={40} imagePath={"/static/images/profile-picture.jpg"}/>
+            </div>
+            <div>
+              <div>name</div>
+              <div>hours</div>
+            </div>
+            <div>dots</div>
+            <div>x icon</div>
+          </div>
+        </div>
+        <div>Body</div>
+        <div>React Box</div>
+        <div>Comment Box</div>
+      </div>
     </CardContainer>
   );
 };
